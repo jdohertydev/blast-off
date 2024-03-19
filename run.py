c1ed5e3e1420e459e22ded6111f0b95f1871b8a5
John Doherty <johndoherty1984@live.com>
2024-03-19 09:19:16 +0000

    Add validate functions for name and question

--- run.py
+++ run.py
@@ -11,24 +11,28 @@ def initiate_game():
 
     print("Welcome to 'Blast Off'!\n")
     print("You have been selected for a very special mission. Before we tell you more...\n")
-    
+
+def validate_name(): 
     while True:
-
         name = input("What is your name? ")
-
         try:
             int(name)
-            print(f"{name} isn't a name on our list, try again")            
-        
+            print(f"{name} isn't a name on our VIP list, try again.")            
+            
         except ValueError:
             print(f"\nThanks, Commander {name}. One more thing. To make sure you are ready for your mission, we have one more question...\n")
-            break
+            return name            
+
+def validate_plants(name):
 
     plant_number = int(input("How many planets are there in our Solar System? "))
-    
-    if plant_number == 9:
-        print("\nWell done! You know your stuff, Commander {name}. It's time to commence the mission")
-    else:
-        print(f"Hmm, {plant_number} isn't quite right, Commander {name}.")
 
+    if plant_number == 9 or plant_number == "nine" or plant_number == "Nine":
+        print(f"\nWell done! You know your stuff, Commander {name}. It's time to commence the mission!")
+    else:
+        print(f"Hmm, {plant_number} isn't quite right, Commander {name}. Try again.")
+
+ 
 initiate_game()
+name = validate_name()
+validate_plants(name)
