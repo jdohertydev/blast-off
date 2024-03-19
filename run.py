7e8e9f651306d0b03eb46ce4bfce257771cc17b1
John Doherty <johndoherty1984@live.com>
2024-03-19 13:51:23 +0000

    Create constants.py. Update typing text code to remove redundant code.

--- run.py
+++ run.py
@@ -23,10 +23,7 @@ def typing_input(text):
     Creates a typing effect to improve user experience.
 
     """
-    for character in text:
-        sys.stdout.write(character)
-        sys.stdout.flush()
-        time.sleep(0.05)
+    typing_print(text)
     value = input()  
     return value
 
